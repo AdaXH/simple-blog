2bd21ac753dc761d5cd56db9a4593ec697e5b529
Ada <3532371088@qq.com>
2022-04-24 11:28:58 +0800

    styles: article detail

--- src/pages/article/detail/index.tsx
+++ src/pages/article/detail/index.tsx
@@ -1,23 +1,34 @@
-import { memo, useState } from 'react';
+import { memo, useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Loading } from '@/component/loading';
 import { Menu } from '@/component/menu';
 import { useLoading, useMount } from '@/util';
 import { queryArticleDetail } from '../service';
+import { DEFAULT_BG } from './constant';
 import './index.less';
 
 export const ArticleDetail: React.FC<{ articleId: string }> = memo(
   ({ articleId }) => {
     const [data, setData] = useState<ArticleModel>();
+    const bgColor = data?.bgColor;
     const [loading, queryArticle] = useLoading(async () => {
       const res = await queryArticleDetail({ _id: articleId });
       setData(res);
     });
     useMount(queryArticle);
     const push = useNavigate();
+    const bgRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      if (bgRef.current && bgColor) {
+        bgRef.current!.style.backgroundColor = bgColor;
+      }
+      return () => {
+        bgRef.current!.style.backgroundColor = DEFAULT_BG;
+      };
+    }, [bgRef.current, bgColor]);
     return (
-      <div className="article global">
-        <div className="global-view article-wrap">
+      <div className="article global" ref={bgRef}>
+        <div className="global-view article-detail-wrap" style={{ backgroundColor: bgColor }}>
           <div className="article-menu">
             <Menu logo="BACK" onClickLogo={() => push('/article')} />
           </div>
--- src/pages/article/type.d.ts
+++ src/pages/article/type.d.ts
@@ -7,4 +7,5 @@ interface ArticleModel {
   type: string;
   viewer: string;
   summary: string;
+  bgColor?: string;
 }
